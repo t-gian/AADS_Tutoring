faa79a4877252b3fd2cb8328fbc2b87d06d04169
gian <94179267+t-gian@users.noreply.github.com>
2023-12-25 02:36:19 +0100

    final commit

--- Exam 24-02-23/Ex12/main.c
+++ Exam 24-02-23/Ex12/main.c
@@ -1,43 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 typedef struct node {
     int key;
     struct node** children;
     int numChildren;
-}node_t;
+} node_t;
 
-
-int compare(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
+void displayTree(node_t* root, int depth, int* depthCounts, node_t*** depthNodes) {
+    depthNodes[depth] = realloc(depthNodes[depth], (depthCounts[depth] + 1) * sizeof(node_t*));
+    depthNodes[depth][depthCounts[depth]] = root;
+    depthCounts[depth]++;
+    for (int i = 0; i < root->numChildren; i++) {
+        displayTree(root->children[i], depth + 1, depthCounts, depthNodes);
+    }
 }
 
-void displayRec(node_t* root, int depth) {
-    if (root == NULL)
-        return;
-    printf("%d ", root->key);
-    qsort(root->children, root->numChildren, sizeof(node_t*), compare);
+int compareNodes(const void* a, const void* b) {
+    node_t* nodeA = *(node_t**)a;
+    node_t* nodeB = *(node_t**)b;
+    return nodeA->key - nodeB->key;
+}
 
-    for (int i = 0; i < root->numChildren; i++) {
-        if (root->children[i] != NULL)
-            printf("%d ", root->children[i]->key);
+void printNodesInOrder(node_t** nodes, int count) {
+    qsort(nodes, count, sizeof(node_t*), compareNodes);
+
+    for (int i = 0; i < count; i++) {
+        printf("%d ", nodes[i]->key);
     }
+}
 
-    for (int i = 0; i < root->numChildren; i++) {
-        displayRec(root->children[i], depth + 1);
+void calculateMaxDepth(node_t* node, int depth, int* maxDepth) {
+    if (node == NULL) {
+        return;
+    }
+    if (depth > *maxDepth) {
+        *maxDepth = depth;
+    }
+    for (int i = 0; i < node->numChildren; i++) {
+        calculateMaxDepth(node->children[i], depth + 1, maxDepth);
     }
 }
 
 void display(node_t* root) {
-    if (root == NULL)
-        return;
-    printf("%d ", root->key);
-    qsort(root->children, root->numChildren, sizeof(node_t*), compare);
+    int maxDepth = 0;
+    calculateMaxDepth(root, 0, &maxDepth);
+    int* depthCounts = calloc(maxDepth + 1, sizeof(int));
+    node_t*** depthNodes = calloc(maxDepth + 1, sizeof(node_t**));
 
-    for (int i = 0; i < root->numChildren; i++) {
-        if (root->children[i] != NULL)
-            printf("%d ", root->children[i]->key);
+    displayTree(root, 0, depthCounts, depthNodes);
+
+    for (int i = 0; i <= maxDepth; i++) {
+        printf("Depth %d: ", i);
+        printNodesInOrder(depthNodes[i], depthCounts[i]);
+        printf("\n");
     }
-    for (int i = 0; i < root->numChildren; i++) {
-        displayRec(root->children[i], 2);
+
+    for (int i = 0; i <= maxDepth; i++) {
+        free(depthNodes[i]);
     }
+    free(depthNodes);
+    free(depthCounts);
 }
